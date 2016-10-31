b99f45e628c749782c47b3c480279f53400b4a32
Nick Beers <nicholas.beers@gmail.com>
2016-10-31 14:28:38 -0400

    Fixing up javascript and adding some CSS styles for buttons and select list

--- src/speechSynth.js
+++ src/speechSynth.js
@@ -5,12 +5,12 @@
 // MIT-style license. Copyright 2016 Nick A. Beers
 //
 ////////////////////////////////////
-(function (window, document, undefined) {
-	"use strict";
-	var closedClass = ' closed';
-    
-	var SpeechSynthProto,
-        SpeechSynth = function (options) {
+(function(window, document, undefined) {
+    "use strict";
+    var closedClass = ' closed';
+
+    var SpeechSynthProto,
+        SpeechSynth = function(options) {
             //Unchanging settings and defaults
             // Default values for some properties
             this.speechSynth = window.speechSynthesis;
@@ -32,15 +32,15 @@
             // set options and create element
             this.setOptions(options);
             this.init();
-	    };
+        };
 
-	//////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////
 
-    (SpeechSynthProto = SpeechSynth.prototype).nothing = function () {};
+    (SpeechSynthProto = SpeechSynth.prototype).nothing = function() {};
 
-	//////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////
 
-    SpeechSynthProto.setOptions = function (options) {
+    SpeechSynthProto.setOptions = function(options) {
         var hasOwnProp = Object.prototype.hasOwnProperty,
             option;
 
@@ -56,19 +56,19 @@
         // set user specified options
         if (options) {
             for (option in this.options) {
-				if (hasOwnProp.call(this.options, option) && options[option] !== undefined) {
-					this.options[option] = options[option];
-				}
-			}
+                if (hasOwnProp.call(this.options, option) && options[option] !== undefined) {
+                    this.options[option] = options[option];
+                }
+            }
         }
     };
 
     //////////////////////////////////////////////////////////////////////////////////
 
-    SpeechSynthProto.speechSynthSpeak = function (text) {
+    SpeechSynthProto.speechSynthSpeak = function(text) {
         if (text) {
             var voices = this.speechSynth.getVoices();
-            
+
             var utterThis = new SpeechSynthesisUtterance(text);
             var selectedOption = this.voiceSelector.selectedOptions[0].getAttribute('data-name');
             for (var i = 0; i < voices.length; i++) {
@@ -84,20 +84,20 @@
             this.speechSynth.speak(utterThis);
         }
     }
-    
-	//////////////////////////////////////////////////////////////////////////////////
-    
-    SpeechSynthProto.generateAudioControls = function () {
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    SpeechSynthProto.generateAudioControls = function() {
         // TODO: Create HTML components and add them into the DOM
         // Allow user to customize classes or provide their own HTML for the controls
         // and specify buttons for each action
-        
+
         // controlLocation: top, bottom, left, right
         // Create container
         this.popoutElement = document.createElement('div');
         this.popoutElement.className = 'speechSynth-container ' + this.options.controlLocation.toLowerCase() + closedClass;
         this.popoutElement.id = 'speechSynth-container';
-        
+
         this.toggleButton = document.createElement('div');
         this.toggleButton.className = 'speechSynth-toggle';
         this.toggleButton.id = 'speechSynth-toggle';
@@ -107,34 +107,29 @@
         var controlsContainer = document.createElement('div');
         controlsContainer.className = 'speechSynth-controls';
 
-        // Label
-        var selectLabel = CreateNewDomElement('label', 'speechSynth-label', 'speechSynth-label');
-        selectLabel.text = 'Language';
-        controlsContainer.appendChild(selectLabel);
-
         // Select
         this.voiceSelector = CreateNewDomElement('select', 'speechSynth-select', 'speechSynth-select');
         controlsContainer.appendChild(this.voiceSelector);
-        
+
         // Play button
-        this.playButton = CreateNewDomElement('button', 'speechsynth-play', 'speechsynth-play');
+        this.playButton = CreateNewDomElement('button', 'speechsynth-play', 'speechsynth-play speechSynth-button');
         this.playButton.textContent = 'Play';
         controlsContainer.appendChild(this.playButton);
 
         // Pause button
-        this.pauseButton = CreateNewDomElement('button', 'speechsynth-pause', 'speechsynth-pause');
+        this.pauseButton = CreateNewDomElement('button', 'speechsynth-pause', 'speechsynth-pause speechSynth-button');
         this.pauseButton.textContent = 'Pause';
         controlsContainer.appendChild(this.pauseButton);
-        
+
         // append controls to container
         this.popoutElement.appendChild(controlsContainer);
         // Add controls to the page
         document.body.appendChild(this.popoutElement);
     };
-    
-	//////////////////////////////////////////////////////////////////////////////////
-    
-    SpeechSynthProto.populateVoiceList = function (self) {
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    SpeechSynthProto.populateVoiceList = function(self) {
         var voices = self.speechSynth.getVoices();
 
         for (var i = 0; i < voices.length; i++) {
@@ -149,7 +144,7 @@
             option.setAttribute('data-name', voices[i].name);
             self.voiceSelector.appendChild(option);
         }
-        
+
         // default needs work to run on all browsers properly
         for (var i = 0; i < this.voiceSelector.options.length; i++) {
             if (self.voiceSelector.options[i].getAttribute('data-name') == self.options.defaultLanguageText) {
@@ -159,9 +154,9 @@
         }
     };
 
-	//////////////////////////////////////////////////////////////////////////////////
-    
-    SpeechSynthProto.addEvents = function ( ) {
+    //////////////////////////////////////////////////////////////////////////////////
+
+    SpeechSynthProto.addEvents = function() {
         var self = this;
         // Add the play button
         self.events.push(self.playButton.addEventListener(self.clickEvt, function(event) {
@@ -171,15 +166,15 @@
             self.speechSynthSpeak(text);
         }));
         // Add the stop button
-        self.events.push(self.pauseButton.addEventListener(self.clickEvt, function(event){
+        self.events.push(self.pauseButton.addEventListener(self.clickEvt, function(event) {
             event.preventDefault();
 
-            if(self.speechSynth.speaking){
+            if (self.speechSynth.speaking) {
                 self.speechSynth.cancel();
             }
         }));
-        
-        self.toggleButton.addEventListener(self.clickEvt, function (event) {
+
+        self.toggleButton.addEventListener(self.clickEvt, function(event) {
             self.isVisible = !self.isVisible;
             if (!self.isVisible) {
                 // Hide the menu
@@ -189,25 +184,26 @@
                 self.popoutElement.className = self.popoutElement.className.replace(closedClass, '');
             }
         });
-        
+
         // setup voice list listener
         if (self.speechSynth.onvoiceschanged !== undefined) {
-            // Needed anonymous function to allow callback to another function with parameters
-            self.speechSynth.onvoiceschanged = function() { self.populateVoiceList(self); };
+            self.speechSynth.onvoiceschanged = function() {
+                self.populateVoiceList(self);
+            };
         }
     };
 
-	//////////////////////////////////////////////////////////////////////////////////
-	
-    SpeechSynthProto.init = function ( ) {
-		// Set up events and load in everything
-        this.generateAudioControls();
-		this.addEvents();
-	};
-    
     //////////////////////////////////////////////////////////////////////////////////
 
-    SpeechSynthProto.getSelectionText = function ( ) {
+    SpeechSynthProto.init = function() {
+        // Set up events and load in everything
+        this.generateAudioControls();
+        this.addEvents();
+    };
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+    SpeechSynthProto.getSelectionText = function() {
         var text = ""
         if (window.getSelection) {
             text = window.getSelection().toString();
@@ -216,28 +212,28 @@
         }
         return text;
     }
-   
-	//////////////////////////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////////////////////////
     //
     //                                 Utility
     //
-	//////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////
 
-    function CreateNewDomElement(elementName, idName, className){
-        if(!elementName){
+    function CreateNewDomElement(elementName, idName, className) {
+        if (!elementName) {
             elementName = 'div'; // Default to div
         }
         var element = document.createElement(elementName);
         if (idName) {
             element.id = idName;
         }
-        if (className) { 
+        if (className) {
             element.className = className;
         }
 
         return element;
-    } 
-        
+    }
+
     // Expose class to user
     window.SpeechSynth = SpeechSynth;
 
